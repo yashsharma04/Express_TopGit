75ead917968f6bab788205c9a32f313e0bb99210
Yash Sharma <95yashsharma@gmail.com>
2017-02-16 14:59:57 +0530

    removed unwanted lines

--- server/app.js
+++ server/app.js
@@ -1,9 +1,7 @@
 var express = require('express');
 var index = require('./config');
-var db = require('./config/database');
 var pg = require('pg');
 var app = express();
-console.log(db);
 require("./config/express")(app);
 require("./routes")(app);
 var sql = require("./sqldb/index.js");
