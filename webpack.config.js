72f56934fa26f63dc7d371d812b0fa784b316d5f
Yash Sharma <95yashsharma@gmail.com>
2017-02-28 16:26:58 +0530

    logger middleware added

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ var DIST_DIR = path.resolve(__dirname, "dist");
 var SRC_DIR = path.resolve(__dirname, "src");
 
 var config = {
-    entry: SRC_DIR + "/app/index.js",
+    entry: SRC_DIR + "/app/index.jsx",
     output: {
         path: DIST_DIR + "/app",
         filename: "bundle.js",
