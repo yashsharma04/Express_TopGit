829f520e707aa90ce9bf93662d129b548b4cf5d9
Yash Sharma <95yashsharma@gmail.com>
2017-03-01 15:41:35 +0530

    intial todo commit

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -1,10 +1,15 @@
-import {render} from "react-dom"
-import React from "react"
-import {Provider} from "react-redux"
-import App from "./containers/App.jsx"
-import store from "./store.jsx"
+import React from 'react'
+import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import todoApp from './reducers'
+import App from './components/App'
+
+let store = createStore(todoApp)
+
 render(
     <Provider store={store}>
         <App />
     </Provider>,
-    window.document.getElementById('app'))
+    document.getElementById('root')
+)
