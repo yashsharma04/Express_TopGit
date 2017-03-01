e51f25e47af9e71ba83e3fa49dca67596432e93c
Yash Sharma <95yashsharma@gmail.com>
2017-03-01 14:30:03 +0530

    async task using promis

--- src/app/actions/userActions.jsx
+++ src/app/actions/userActions.jsx
@@ -1,7 +1,22 @@
 export function setName(name){
+    // return dispatch => {
+    //     setTimeout(
+    //         () => {
+    //             dispatch({
+    //                 type:"SET_NAME",
+    //                 payload:name
+    //             })
+    //         }
+    //     )
+    // }
     return {
-        type : "SET_NAME",
-        payload: name
+        type:"SET_NAME",
+        payload: new Promise((resolve,reject) => {
+            setTimeout(() => {
+                resolve(name)
+            },2000)
+        })
+
     }
 }
 
--- src/app/store.jsx
+++ src/app/store.jsx
@@ -2,11 +2,14 @@ import {createStore , combineReducers,applyMiddleware} from "redux"
 import logger from "redux-logger"
 import math from "./reducers/mathReducer.jsx"
 import user from "./reducers/userReducer.jsx"
+import thunk from "redux-thunk"
+import promise from "redux-promise-middleware"
+
 export default createStore(
     combineReducers({
         math,
         user
     }),
     {},
-    applyMiddleware(logger())
+    applyMiddleware(logger(),thunk,promise())
 );
